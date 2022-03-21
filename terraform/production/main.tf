81dec2536897157f6545b183047288c564dd6752
Andrew White <andrew.white@unboxed.co>
2022-03-21 07:49:16 +0000

    Initial infrastructure and deployment pipeline

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,62 +1,90 @@
-# INSTRUCTIONS: 
-# 1) ENSURE YOU POPULATE THE LOCALS 
-# 2) ENSURE YOU REPLACE ALL INPUT PARAMETERS, THAT CURRENTLY STATE 'ENTER VALUE', WITH VALID VALUES 
-# 3) YOUR CODE WOULD NOT COMPILE IF STEP NUMBER 2 IS NOT PERFORMED!
-# 4) ENSURE YOU CREATE A BUCKET FOR YOUR STATE FILE AND YOU ADD THE NAME BELOW - MAINTAINING THE STATE OF THE INFRASTRUCTURE YOU CREATE IS ESSENTIAL - FOR APIS, THE BUCKETS ALREADY EXIST
-# 5) THE VALUES OF THE COMMON COMPONENTS THAT YOU WILL NEED ARE PROVIDED IN THE COMMENTS
-# 6) IF ADDITIONAL RESOURCES ARE REQUIRED BY YOUR API, ADD THEM TO THIS FILE
-# 7) ENSURE THIS FILE IS PLACED WITHIN A 'terraform' FOLDER LOCATED AT THE ROOT PROJECT DIRECTORY
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+
+  backend "s3" {
+    bucket  = "lbh-mosaic-terraform-state-production"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/brokerage-api/state"
+  }
+}
 
 provider "aws" {
   region  = "eu-west-2"
-  version = "~> 2.0"
 }
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
 locals {
-  application_name = your application name # The name to use for your application
-   parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+  environment     = "production"
+  application     = "brokerage-api"
+  aws_account     = data.aws_caller_identity.current.account_id
+  aws_region      = data.aws_region.current.name
+  parameter_store = "arn:aws:ssm:${local.aws_region}:${local.aws_account}:parameter"
+
+  db_name = "brokerage-api-db-production"
+  db_port = "5432"
 }
 
-
-data "aws_iam_role" "ec2_container_service_role" {
-  name = "ecsServiceRole"
-}
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-production-apis"
-    encrypt = true
-    region  = "eu-west-2"
-    key     = services/YOUR API NAME/state #e.g. "services/transactions-api/state"
+data "aws_vpc" "default" {
+  tags = {
+    Name = "Mosaic-prod"
   }
 }
 
-module "development" {
-  # Delete as appropriate:
-  source                      = "github.com/LBHackney-IT/aws-hackney-components-per-service-terraform.git//modules/environment/backend/fargate"
-  # source = "github.com/LBHackney-IT/aws-hackney-components-per-service-terraform.git//modules/environment/backend/ec2"
-  cluster_name                = "production-apis"
-  ecr_name                    = ecr repository name # Replace with your repository name - pattern: "hackney/YOUR APP NAME"
-  environment_name            = "production"
-  application_name            = local.application_name 
-  security_group_name         = back end security group name # Replace with your security group name, WITHOUT SPECIFYING environment. Usually the SG has the name of your API
-  vpc_name                    = "vpc-production-apis"
-  host_port                   = port # Replace with the port to use for your api / app
-  port                        = port # Replace with the port to use for your api / app
-  desired_number_of_ec2_nodes = number of nodes # Variable will only be used if EC2 is required. Do not remove it. 
-  lb_prefix                   = "nlb-production-apis"
-  ecs_execution_role          = data.aws_iam_role.ecs_task_execution_role.arn
-  lb_iam_role_arn             = data.aws_iam_role.ec2_container_service_role.arn
-  task_definition_environment_variables = {
-    ASPNETCORE_ENVIRONMENT = "production"
+data "aws_subnet_ids" "private_subnets" {
+  vpc_id = data.aws_vpc.default.id
+
+  tags = {
+    Type = "private"
   }
-  task_definition_environment_variable_count = number # This number needs to reflect the number of environment variables provided
-  cost_code = your project's cost code
-  task_definition_secrets      = {}
-  task_definition_secret_count = number # This number needs to reflect the number of environment variables provided
+}
+
+data "aws_ssm_parameter" "postgres_password" {
+  name = "/${local.application}/${local.environment}/postgres-password"
+}
+
+data "aws_ssm_parameter" "postgres_username" {
+  name = "/${local.application}/${local.environment}/postgres-username"
+}
+
+module "database" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
+  environment_name = local.environment
+  vpc_id = data.aws_vpc.default.id
+  db_identifier = local.application
+  db_name = local.db_name
+  db_port = local.db_port
+  subnet_ids = data.aws_subnet_ids.private_subnets.ids
+  db_engine = "postgres"
+  db_engine_version = "12.10"
+  db_instance_class = "db.t3.small"
+  db_allocated_storage = 20
+  maintenance_window = "sun:04:00-sun:04:30"
+  db_username = data.aws_ssm_parameter.postgres_username.value
+  db_password = data.aws_ssm_parameter.postgres_password.value
+  storage_encrypted = true
+  multi_az = local.environment == "production"
+  publicly_accessible = false
+  project_name = "brokerage"
+}
+
+resource "aws_ssm_parameter" "database_hostname" {
+  name        = "/${local.application}/${local.environment}/postgres-hostname"
+  type        = "String"
+  value       = aws_db_instance.database.address
+}
+
+resource "aws_ssm_parameter" "database_port" {
+  name        = "/${local.application}/${local.environment}/postgres-port"
+  type        = "String"
+  value       = aws_db_instance.database.port
 }
