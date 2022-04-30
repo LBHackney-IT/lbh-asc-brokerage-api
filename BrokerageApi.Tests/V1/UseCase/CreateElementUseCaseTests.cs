53935a1e30a179e2a9544d79dad12730a38f45f9
Andrew White <andrew.white@unboxed.co>
2022-04-30 07:16:20 +0100

    Add extra tests for use case exceptions

--- BrokerageApi.Tests/V1/UseCase/CreateElementUseCaseTests.cs
+++ BrokerageApi.Tests/V1/UseCase/CreateElementUseCaseTests.cs
@@ -89,5 +89,141 @@ namespace BrokerageApi.Tests.V1.UseCase
             _providerGatewayMock.Verify(m => m.GetByIdAsync(provider.Id));
             _dbSaverMock.Verify(x => x.SaveChangesAsync(), Times.Once());
         }
+
+        [Test]
+        public void ThrowsArgumentNullExceptionWhenReferralDoesntExist()
+        {
+            // Arrange
+            var request = _fixture.Create<CreateElementRequest>();
+
+            _referralGatewayMock.Setup(x => x.GetByIdAsync(123456))
+                .ReturnsAsync(null as Referral);
+
+            _userServiceMock.SetupGet(x => x.Name)
+                .Returns("a.broker@hackney.gov.uk");
+
+            // Act
+            var exception = Assert.ThrowsAsync<ArgumentNullException>(
+                async () => await _classUnderTest.ExecuteAsync(123456, request));
+
+            // Assert
+            Assert.That(exception.Message, Is.EqualTo("Referral not found for: 123456 (Parameter 'referralId')"));
+        }
+
+        [Test]
+        public void ThrowsInvalidOperationExceptionWhenReferralIsNotInProgress()
+        {
+            // Arrange
+            var request = _fixture.Create<CreateElementRequest>();
+
+            var referral = _fixture.Build<Referral>()
+                .With(x => x.Status, ReferralStatus.Assigned)
+                .With(x => x.AssignedTo, "a.broker@hackney.go.uk")
+                .Create();
+
+            _referralGatewayMock.Setup(x => x.GetByIdAsync(referral.Id))
+                .ReturnsAsync(referral);
+
+            _userServiceMock.SetupGet(x => x.Name)
+                .Returns("a.broker@hackney.gov.uk");
+
+            // Act
+            var exception = Assert.ThrowsAsync<InvalidOperationException>(
+                async () => await _classUnderTest.ExecuteAsync(referral.Id, request));
+
+            // Assert
+            Assert.That(exception.Message, Is.EqualTo("Referral is not in a valid state for editing"));
+        }
+
+        [Test]
+        public void ThrowsUnauthorizedAccessExceptionWhenReferralIsAssignedToSomeoneElse()
+        {
+            // Arrange
+            var request = _fixture.Create<CreateElementRequest>();
+
+            var referral = _fixture.Build<Referral>()
+                .With(x => x.Status, ReferralStatus.InProgress)
+                .With(x => x.AssignedTo, "other.broker@hackney.gov.uk")
+                .Create();
+
+            _referralGatewayMock.Setup(x => x.GetByIdAsync(referral.Id))
+                .ReturnsAsync(referral);
+
+            _userServiceMock.SetupGet(x => x.Name)
+                .Returns("a.broker@hackney.gov.uk");
+
+            // Act
+            var exception = Assert.ThrowsAsync<UnauthorizedAccessException>(
+                async () => await _classUnderTest.ExecuteAsync(referral.Id, request));
+
+            // Assert
+            Assert.That(exception.Message, Is.EqualTo("Referral is not assigned to a.broker@hackney.gov.uk"));
+        }
+
+        [Test]
+        public void ThrowsArgumentExceptionWhenElementTypeDoesNotExist()
+        {
+            // Arrange
+            var request = _fixture.Build<CreateElementRequest>()
+                .With(x => x.ElementTypeId, 123456)
+                .Create();
+
+            var referral = _fixture.Build<Referral>()
+                .With(x => x.Status, ReferralStatus.InProgress)
+                .With(x => x.AssignedTo, "a.broker@hackney.gov.uk")
+                .Create();
+
+            _referralGatewayMock.Setup(x => x.GetByIdAsync(referral.Id))
+                .ReturnsAsync(referral);
+
+            _userServiceMock.SetupGet(x => x.Name)
+                .Returns("a.broker@hackney.gov.uk");
+
+            _elementTypeGatewayMock.Setup(x => x.GetByIdAsync(123456))
+                .ReturnsAsync(null as ElementType);
+
+            // Act
+            var exception = Assert.ThrowsAsync<ArgumentException>(
+                async () => await _classUnderTest.ExecuteAsync(referral.Id, request));
+
+            // Assert
+            Assert.That(exception.Message, Is.EqualTo("Element type not found for: 123456"));
+        }
+
+        [Test]
+        public void ThrowsArgumentExceptionWhenProviderDoesNotExist()
+        {
+            // Arrange
+            var elementType = _fixture.Create<ElementType>();
+
+            var request = _fixture.Build<CreateElementRequest>()
+                .With(x => x.ElementTypeId, elementType.Id)
+                .With(x => x.ProviderId, 123456)
+                .Create();
+
+            var referral = _fixture.Build<Referral>()
+                .With(x => x.Status, ReferralStatus.InProgress)
+                .With(x => x.AssignedTo, "a.broker@hackney.gov.uk")
+                .Create();
+
+            _referralGatewayMock.Setup(x => x.GetByIdAsync(referral.Id))
+                .ReturnsAsync(referral);
+
+            _userServiceMock.SetupGet(x => x.Name)
+                .Returns("a.broker@hackney.gov.uk");
+
+            _elementTypeGatewayMock.Setup(x => x.GetByIdAsync(elementType.Id))
+                .ReturnsAsync(elementType);
+
+            _providerGatewayMock.Setup(x => x.GetByIdAsync(123456))
+                .ReturnsAsync(null as Provider);
+
+            // Act
+            var exception = Assert.ThrowsAsync<ArgumentException>(
+                async () => await _classUnderTest.ExecuteAsync(referral.Id, request));
+
+            // Assert
+            Assert.That(exception.Message, Is.EqualTo("Provider not found for: 123456"));
+        }
     }
 }
